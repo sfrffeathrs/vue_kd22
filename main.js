4a6f1070006b0cf29cc4605913293a97496c34f3
root <root@blue.localdomain>
2022-10-04 10:03:26 +0000

    attribute binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: 'Shoes',
-			description: 'Shoes.'
+			image: './assets/images/sneakers_green.png',
+			url: 'https://jisho.org/'
 		}
 	}
 })
