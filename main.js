85f4879e315b2f26d1ca6bf9f4aa343d33af5396
root <root@blue.localdomain>
2022-10-04 13:53:35 +0000

    class and style binding

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ const app = Vue.createApp({
 			cart: 0,
 			product: 'Shoes',
 			image: './assets/images/sneakers_green.png',
-			inventory: 8,
-			onSale: true,
+			inStock: false,
 			details: ['50% cotton', '30% wool', '20% polyester'],
-			sizes: ['S', 'M', 'L'],
 			variants: [
 				{ id: 2234, color: 'green', image: './assets/images/sneakers_green.png' },
 				{ id: 2235, color: 'blue', image: './assets/images/sneakers_blue.png' },
