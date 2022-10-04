486b33e6240a7db0417b3d67853d75284736559b
root <root@blue.localdomain>
2022-10-04 10:33:52 +0000

    list rendering

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
 			product: 'Shoes',
 			image: './assets/images/sneakers_green.png',
 			inventory: 8,
-			onSale: true
+			onSale: true,
+			details: ['50% cotton', '30% wool', '20% polyester'],
+			sizes: ['S', 'M', 'L'],
+			variants: [
+				{ id: 2234, color: 'green' },
+				{ id: 2235, color: 'blue' },
+			]
 		}
 	}
 })
