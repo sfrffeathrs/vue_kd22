9f50bdf2d6a6dc6ebdfd6b43324f5d8d39e40dec
root <root@blue.localdomain>
2022-10-04 12:46:08 +0000

    event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			cart: 0,
 			product: 'Shoes',
 			image: './assets/images/sneakers_green.png',
 			inventory: 8,
@@ -8,9 +9,20 @@ const app = Vue.createApp({
 			details: ['50% cotton', '30% wool', '20% polyester'],
 			sizes: ['S', 'M', 'L'],
 			variants: [
-				{ id: 2234, color: 'green' },
-				{ id: 2235, color: 'blue' },
+				{ id: 2234, color: 'green', image: './assets/images/sneakers_green.png' },
+				{ id: 2235, color: 'blue', image: './assets/images/sneakers_blue.png' },
 			]
+		}
+	},
+	methods: {
+		addToCart() {
+			this.cart +=1
+		},
+		removeFromCart() {
+			this.cart -=1
+		},
+		updateImage(variantImage) {
+			this.image = variantImage
 		}
 	}
 })
